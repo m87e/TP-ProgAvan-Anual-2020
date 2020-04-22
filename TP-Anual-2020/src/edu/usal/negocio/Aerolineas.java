4f1882c5f19a3c9af18674fb5110a144cfaa00ac
matia <matia@DESKTOP-B53N70N>
2020-04-22 20:36:34 -0300

    Segundo Commit

---
+++ TP-Anual-2020/src/edu/usal/negocio/Aerolineas.java
@@ -0,0 +1,15 @@
+package edu.usal.negocio;
+
+public class Aerolineas {
+	private int nroVue;
+
+	public int getNroVue() {
+		return nroVue;
+	}
+
+	public void setNroVue(int nroVue) {
+		this.nroVue = nroVue;
+	}
+	
+	
+}
