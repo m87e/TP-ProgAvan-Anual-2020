8f6bd9cf9a8bbacc5c74e17fc134632f737c4471
juanigonzalez <juanigonzalez@localhost>
2020-04-22 21:13:35 -0300

    Commit

--- TP-Anual-2020/src/edu/usal/negocio/Cliente.java
+++ TP-Anual-2020/src/edu/usal/negocio/Cliente.java
@@ -4,4 +4,27 @@ public class Cliente {
 
 	private String nombre;
 	private String apellido;
+	
+	public Cliente() {
+		
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	
+	
+	
 }
