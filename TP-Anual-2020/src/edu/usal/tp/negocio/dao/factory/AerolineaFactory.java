ce449c6502c4175f16f81d2e69211869300f876c
Magui <Magui@192.168.1.30>
2020-08-13 20:31:10 -0300

    AerolineaFactory database implementation update

--- TP-Anual-2020/src/edu/usal/tp/negocio/dao/factory/AerolineaFactory.java
+++ TP-Anual-2020/src/edu/usal/tp/negocio/dao/factory/AerolineaFactory.java
@@ -1,15 +1,19 @@
 package edu.usal.tp.negocio.dao.factory;
 
 import edu.usal.tp.negocio.dao.implementaciones.AerolineaDAOImplArchivo;
+import edu.usal.tp.negocio.dao.implementaciones.AerolineaDAOImplDatabase;
 import edu.usal.tp.negocio.dao.interfaces.IAerolineaDAO;
 
 public class AerolineaFactory {
-	public static IAerolineaDAO GetImplementatios (String source) {
+	public static IAerolineaDAO GetImplementatios(String source) {
 		if (source.equals("Archivo")) {
 			return new AerolineaDAOImplArchivo();
 		}
-		
+		if (source.equals("database")) {
+			return new AerolineaDAOImplDatabase();
+		}
+
 		return null;
-		
+
 	}
 }
