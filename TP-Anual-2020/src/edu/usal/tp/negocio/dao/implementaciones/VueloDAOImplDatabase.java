8706c21bbb2ca442cdf33b3f98cd94897dfc3bcb
Magui <Magui@192.168.1.30>
2020-08-19 15:17:54 -0300

    agrego vueloDAOImplDatabase

---
+++ TP-Anual-2020/src/edu/usal/tp/negocio/dao/implementaciones/VueloDAOImplDatabase.java
@@ -0,0 +1,36 @@
+package edu.usal.tp.negocio.dao.implementaciones;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.List;
+
+import edu.usal.tp.negocio.dao.dominio.Vuelos;
+import edu.usal.tp.negocio.dao.interfaces.IVuelosDAO;
+
+public class VueloDAOImplDatabase implements IVuelosDAO {
+
+	@Override
+	public void AgregarVuelo(Vuelos vuelos) throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void ModificarVuelo(Vuelos vuelos) throws IOException, ParseException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void EliminarVuelo(Vuelos vuelos) throws IOException, ParseException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Vuelos> GetAll() throws IOException, ParseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
