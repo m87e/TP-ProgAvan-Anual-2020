261676a5fd42bd9dfe2c1a43befc1e93c0db67dd
juanigonzalez <juanigonzalez@localhost>
2020-08-27 16:18:10 -0300

    update

--- TP-Anual-2020-DAO/src/edu/usal/tp/negocio/dao/factory/TelefonoFactory.java
+++ TP-Anual-2020-DAO/src/edu/usal/tp/negocio/dao/factory/TelefonoFactory.java
@@ -1,13 +1,18 @@
 package edu.usal.tp.negocio.dao.factory;
 
 import edu.usal.tp.negocio.dao.implementaciones.TelefonoDAOImpArchivo;
+import edu.usal.tp.negocio.dao.implementaciones.TelefonoDAOImplDatabase;
 import edu.usal.tp.negocio.dao.interfaces.ITelefonoDAO;
 
 public class TelefonoFactory {
-public static ITelefonoDAO GetImplementation (String source) {
-		
-		if (source.equals("Archivo")){
+	public static ITelefonoDAO GetImplementation(String source) {
+
+		if (source.equals("Archivo")) {
 			return new TelefonoDAOImpArchivo();
+		}
+
+		if (source.equals("database")) {
+			return new TelefonoDAOImplDatabase();
 		}
 		return null;
 	}
