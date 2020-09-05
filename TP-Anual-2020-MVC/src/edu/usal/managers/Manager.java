7aa03219b7c03f8bbddb560e7941033804a27ee4
juanigonzalez <juanigonzalez@localhost>
2020-09-05 09:26:26 -0300

    primeras lineas del manager

--- TP-Anual-2020-MVC/src/edu/usal/managers/Manager.java
+++ TP-Anual-2020-MVC/src/edu/usal/managers/Manager.java
@@ -6,8 +6,15 @@ public class Manager {
 
 		// Cliente c = new Cliente();
 
+		//c.setNombre(nombre);
+		
+		
 		// Pasaporte p = ObtenerPasaportePorNumero(numeroPasaporte);
 		// c.setPas(p);
+		
+		
+		//AGREGAR LO DEL COMMIT
+		//this.daoDatabase.AgregarCliente(c);
 
 	}
 
